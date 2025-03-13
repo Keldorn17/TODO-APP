5cc1a0e1799766b5fedbeb74e01d8073e5f1806b
Keldorn17 <patai17.zoltan17@gmail.com>
2025-03-13 20:55:43 +0100

    Replaced bool with Priority Object

--- MVVM/ViewModel/HomeViewModel.cs
+++ MVVM/ViewModel/HomeViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using TODO.Core;
 using TODO.MVVM.Model;
 
@@ -20,17 +21,25 @@ namespace TODO.MVVM.ViewModel
         public HomeViewModel()
         {
             TodoItems = new ObservableCollection<TodoItem>();
-            AddTodoItem(1, "What is Lorem Ipsum?", "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", true);
-            AddTodoItem(2, "Why do we use it?", "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.", false, true);
-            AddTodoItem(3, "Where does it come from?", "The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.");
-            AddTodoItem(4, "Where can I get some?", "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.", true, true);
+            try
+            {
+                AddTodoItem(1, "What is Lorem Ipsum?", "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", new Priority { Level = 1 });
+                AddTodoItem(2, "Why do we use it?", "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.", new Priority { Level = 0 }, true);
+                AddTodoItem(3, "Where does it come from?", "The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.", new Priority { Level = 2 });
+                AddTodoItem(4, "Where can I get some?", "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.", new Priority { Level = 3 }, true);
+                AddTodoItem(5, "What is Lorem Ipsum?", "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", new Priority { Level = 4 });
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
         }
 
         public void AddTodoItem(
             long id,
             string title,
             string description,
-            bool priority = false,
+            Priority priority,
             bool isCompleted = false,
             string owner = null,
             string deadline = null,
