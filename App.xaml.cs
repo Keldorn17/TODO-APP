533b91e9d162aae2ca9a12d337a6d3b84ee6f5fc
Zoltán Patai <113589766+Keldorn17@users.noreply.github.com>
2025-04-09 18:11:35 +0200

    Feature/cicd (#21)
    * CI/CD for release
    
    * Working CI/CD

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,5 +9,18 @@ namespace TODO;
 /// </summary>
 public partial class App : Application
 {
+    public App()
+    {
+        // Attach the handler for unhandled exceptions
+        this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+    }
+
+    // This will handle unhandled exceptions in the app
+    private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+    {
+        // Show an error message or log the exception details
+        MessageBox.Show($"Unhandled exception: {e.Exception.Message}");
+        e.Handled = true; // Prevents the application from crashing
+    }
 }
 
